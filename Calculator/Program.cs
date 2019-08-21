b9e29c2a44ece0a7b463213bef14d5964d97ce6e
Matthew John Ragas <mjrag310@hotmail.com>
2019-08-21 17:53:51 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        int sum;
+                
+
+        static void Main(string[] args)
+        {
+            string quitCom = "";
+            string action = "";
+            bool quit = true;
+            while ( quit == true)
+            {
+
+                Console.WriteLine("if you wish to quit press x.");
+            }
+
+            
+        }
+
+        int Addition(ref int firstNum , ref int secondNum)
+        {
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            secondNum = Convert.ToInt32(Console.ReadLine());
+            sum = firstNum + secondNum;
+            return sum;
+        }
+
+        int Subtraction (ref int firstNum, ref int secondNum)
+        {
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            sum = firstNum - secondNum;
+            return sum;
+        }
+
+        int Division( ref int firstNum, ref int secondNum)
+        {
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            sum = firstNum / secondNum;
+            return sum;
+        }
+       
+        int Multiplication(ref int firstNum, ref int secondNum)
+        {
+            firstNum = Convert.ToInt32(Console.ReadLine());
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            sum = firstNum * secondNum;
+            return sum;
+        }
+
+        bool Quit(string quitCom)
+        {
+            if (quitCom == "")
+            {
+                return false;
+            }    
+
+            return true;
+        }
+    }
+}
