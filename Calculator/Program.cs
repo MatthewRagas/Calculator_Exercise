1eae6f25b8cf578bb8ee2cea745d9db391fce35a
Matthew John Ragas <mjrag310@hotmail.com>
2019-08-22 12:48:26 -0500

    Constructed code for Calculator using variables, funcitons, loops, and conditions. Gave a quit option.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,66 +8,99 @@ namespace Calculator
 {
     class Program
     {
-        int sum;
-                
 
         static void Main(string[] args)
         {
-            string quitCom = "";
-            string action = "";
             bool quit = true;
-            while ( quit == true)
+            string stop = "";
+            int numThree = 0;
+                        
+
+            while(quit == true)
             {
 
-                Console.WriteLine("if you wish to quit press x.");
+                Console.WriteLine("Please input your equation.\nYour choice of operations are *, /, +, -.");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                string operation = Console.ReadLine();
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                Console.ReadKey();
+
+                if (operation == "+")
+                {
+                   numThree = Addition(numOne, numTwo);
+
+                }
+
+                else if (operation == "-")
+                {
+                    numThree = Subtraction(numOne, numTwo);
+                }
+
+                else if( operation == "*")
+                {
+                    numThree = Multiplication(numOne, numTwo);
+                }
+
+                else if(operation == "/")
+                {
+                    numThree = Division(numOne, numTwo);
+                }
+
+                //displays sum of equation
+                Console.WriteLine(numThree);
+
+                Console.WriteLine("If you wish to exit\nthe application type yes\nif no, type no");
+                stop = Console.ReadLine();
+
+                if (stop == "yes" || stop == "Yes")
+                {
+                    Quit();
+                }
+                else
+                {
+                    quit = true;
+                }
+               
+
             }
 
             
-        }
 
-        int Addition(ref int firstNum , ref int secondNum)
-        {
-            firstNum = Convert.ToInt32(Console.ReadLine());
-            secondNum = Convert.ToInt32(Console.ReadLine());
-            sum = firstNum + secondNum;
-            return sum;
-        }
-
-        int Subtraction (ref int firstNum, ref int secondNum)
-        {
-            firstNum = Convert.ToInt32(Console.ReadLine());
-            secondNum = Convert.ToInt32(Console.ReadLine());
-
-            sum = firstNum - secondNum;
-            return sum;
-        }
-
-        int Division( ref int firstNum, ref int secondNum)
-        {
-            firstNum = Convert.ToInt32(Console.ReadLine());
-            secondNum = Convert.ToInt32(Console.ReadLine());
-
-            sum = firstNum / secondNum;
-            return sum;
-        }
-       
-        int Multiplication(ref int firstNum, ref int secondNum)
-        {
-            firstNum = Convert.ToInt32(Console.ReadLine());
-            secondNum = Convert.ToInt32(Console.ReadLine());
-
-            sum = firstNum * secondNum;
-            return sum;
-        }
-
-        bool Quit(string quitCom)
-        {
-            if (quitCom == "")
+            //Addition function;
+            int Addition(int lhs, int rhs)
             {
-                return false;
-            }    
+                int sum = lhs + rhs;
+                return sum;
+            }
 
-            return true;
-        }
+            //Subtraction function;
+            int Subtraction(int lhs, int rhs)
+            {
+                int sum = lhs + rhs;
+                return sum;
+            }
+
+            //Multiplication function;
+            int Multiplication(int lhs, int rhs)
+            {
+                int sum = lhs * rhs;
+                return sum;
+            }
+
+            //Division functioin;
+            int Division(int lhs, int rhs)
+            {
+                int sum = lhs / rhs;
+                return sum;
+            }
+            
+            //Quit program functioin;
+            bool Quit()
+            {
+                quit = false;
+                return quit;
+            }
+        }       
+
     }
 }
